96b0851b5cf3ed3ed60029549dbb0fa75ed9d61d
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-30 17:00:01 +0100

    Add function

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,21 +1,24 @@
 
+function createSquareElement(content, classes){
+    const square = document.createElement('div');
+    square.innerText = content;
+    square.classList.add(classes,  "d-flex", "justify-content-center", "align-items-center", "fs-5", "p-3")
+    square.addEventListener('click', function(){
+        square.classList.toggle("click")
+    })
+    return square;
+}
 
 
 
 
-
-const playButton = document.querySelector('a.btn')
+const playButton = document.querySelector('a.btn');
 const containerElement = document.querySelector('div.big-square');
+const newSquare = createSquareElement();
 
 playButton.addEventListener('click', function(){
     containerElement.innerHTML = ' ';
     for(let i = 1; i < 101; i++){
-        const newDivElement = document.createElement('div');
-        newDivElement.classList.add("square", "d-flex", "justify-content-center", "align-items-center");
-        newDivElement.innerText = i;
-        containerElement.appendChild(newDivElement);
-        newDivElement.addEventListener('click', function(){
-            newDivElement.classList.toggle("click")
-        })
+        containerElement.appendChild(createSquareElement(i, 'square'));
     }
 })
