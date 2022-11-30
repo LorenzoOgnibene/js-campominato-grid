5c4efba53ed1cd55d7b5ac172d8d7f42e9e8defa
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2022-11-30 15:36:38 +0100

    Add second color on click

--- js/javaScript.js
+++ js/javaScript.js
@@ -8,9 +8,14 @@ const playButton = document.querySelector('a.btn')
 const containerElement = document.querySelector('div.big-square');
 
 playButton.addEventListener('click', function(){
-    for(let i = 0; i < 100; i++){
+    containerElement.innerHTML = ' ';
+    for(let i = 1; i < 101; i++){
         const newDivElement = document.createElement('div');
-        newDivElement.classList.add('square');
+        newDivElement.classList.add("square", "d-flex", "justify-content-center", "align-items-center");
+        newDivElement.innerText = i;
         containerElement.appendChild(newDivElement);
+        newDivElement.addEventListener('click', function(){
+            newDivElement.classList.toggle("click")
+        })
     }
 })
